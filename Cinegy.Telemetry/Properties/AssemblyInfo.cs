a0dae8fc81c0ec86196d0d5ac8ef5beb2c871f2e
Lewis Kirkaldie <lewis.kirkaldie@gmail.com>
2018-02-19 19:49:34 +0000

    -change to VS2017 with NET Framework 4.6.1 and NET Standard 2.0 multi targetting

--- Cinegy.Telemetry/Properties/AssemblyInfo.cs
+++ Cinegy.Telemetry/Properties/AssemblyInfo.cs
@@ -6,15 +6,15 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Cinegy.Telemetry")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Cinegy.Telemetry")]
-[assembly: AssemblyCopyright("Copyright © Cinegy GmbH 2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+//[assembly: AssemblyTitle("Cinegy.Telemetry")]
+//[assembly: AssemblyDescription("")]
+//[assembly: AssemblyConfiguration("")]
+//[assembly: AssemblyCompany("")]
+//[assembly: AssemblyProduct("Cinegy.Telemetry")]
+//[assembly: AssemblyCopyright("Copyright © Cinegy GmbH 2017")]
+//[assembly: AssemblyTrademark("")]
+//[assembly: AssemblyCulture("")]
+//[assembly: NeutralResourcesLanguage("en")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -25,7 +25,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0-beta")]
+//// [assembly: AssemblyVersion("1.0.*")]
+//[assembly: AssemblyVersion("2.0.0.0")]
+//[assembly: AssemblyFileVersion("2.0.0.0")]
+//[assembly: AssemblyInformationalVersion("2.0.0-beta")]
